ac0cc7b87b4bb83e9985bc431b535843aa398c76
Rohit Tiwari <www.rohiktiwari771@gmail.com>
2024-05-19 17:45:56 +0530

    fixed h1

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import '../../App.css';
 
 function Services() {
-  return(
-  <>
-  
-   <h1 className='services'>Hello
-   <p>Talent Glimpse is like a platform where Students (especially teen age) get an ecosystem for 
-their career, since teen age is Golden Stage of life, where the career is highly depended where 
-only possibility to make your life a successful or struggler or failure
-We are positively doing well in such a way, and getting positive response from users</p>
-   </h1>;
-   
-  </>
-  );
+  return <h1 className='services'>Hello</h1>;
 }
 
 export default Services;
