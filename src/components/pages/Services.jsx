5850b16c2e1ce2ca18ab642e5b36640bcea8c851
Rohit Tiwari <www.rohiktiwari771@gmail.com>
2024-05-19 17:39:20 +0530

    added title

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import '../../App.css';
 
 function Services() {
+  return(
   <>
-  return
+  
    <h1 className='services'>Hello</h1>;
    <p>Talent Glimpse is like a platform where Students (especially teen age) get an ecosystem for 
 their career, since teen age is Golden Stage of life, where the career is highly depended where 
 only possibility to make your life a successful or struggler or failure
 We are positively doing well in such a way, and getting positive response from users</p>
   </>
+  );
 }
 
 export default Services;
