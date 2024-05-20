6cb087dd6ab838163dc667eb6bf0a9e364bb2a71
Rohit Tiwari <www.rohiktiwari771@gmail.com>
2024-05-21 00:23:10 +0530

    Added more text

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -2,5 +2,14 @@ import React from 'react';
 import '../../App.css';
 
 export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+  return (
+    <div className="products-container">
+      <h1 className="products-title">More at TG</h1>
+      <ul className="products-list">
+        <li>Theme Innovation TG Olympiad</li>
+        <li>Exam Interaction with Experts</li>
+        <li>Seminars / Webinar</li>
+      </ul>
+    </div>
+  );
 }
